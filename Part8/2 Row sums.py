53c83d2a8fe359959a0df72c4bd880be618d3855
BassWoodSucksAtCoding <thebasswoodwhocriedpancake@gmail.com>
2026-06-25 18:46:01 +0600

    Create 2 Row sums.py

---
+++ Part8/2 Row sums.py
@@ -0,0 +1,8 @@
+# Write your solution here
+def row_sums(my_matrix: list):
+    for row in my_matrix:
+        row.append(sum(row))
+    
+# my_matrix = [[1, 2], [3, 4]]
+# row_sums(my_matrix)
+# print(my_matrix)
